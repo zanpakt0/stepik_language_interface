d5b59a0cf9f1f7fab97db99768751d2031d1c2d6
zanpakt0 <zhukov.k.a.1992@gmail.com>
2019-07-31 22:25:20 +0300

    cosmetic changes

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
-
 def test_exist_button_add_to_basket(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary"), "Кнопка добавления в корзину отсутствует"
