87f79d2fea238b30ae143c9ec82746651b7f9773
zanpakt0 <zhukov.k.a.1992@gmail.com>
2019-07-31 21:42:36 +0300

    Add test for button and conftest

---
+++ test_items.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
+
+def test_exist_button_add_to_basket(browser):
+    browser.get(link)
+    assert browser.find_element_by_css_selector(".btn.btn-lg.btn-primary"), "Кнопка добавления в корзину отсутствует"
